afbaf33257f1075d39d57691d2d564131dcd29a2
Michal3456 <troianorum@wp.pl>
2017-11-16 20:57:28 +0100

    Zmiana test.

--- Sklep/Koszyk_Panel.cs
+++ Sklep/Koszyk_Panel.cs
@@ -6,14 +6,13 @@ using System.Threading.Tasks;
 
 namespace Sklep
 {
-    class Koszyk
+    class Koszyk_Panel
     {
-        
         public static void zapis_kosz(float cena, int liczba_przed)
         {
             Console.WriteLine("************************");
             Console.WriteLine("************************");
-            Console.WriteLine($"Kosz: {cena : F2} zł");
+            Console.WriteLine($"Kosz: {cena: F2} zł");
             Console.WriteLine("************************");
             Console.WriteLine("************************");
             Console.WriteLine("************************");
